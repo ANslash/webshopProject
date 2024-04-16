d9f23eadfde6028eafe8f8951541711e0d51499f
ANslash <75243928+ANslash@users.noreply.github.com>
2024-04-16 10:11:12 +0200

    Add files via upload

--- user.py
+++ user.py
@@ -1,10 +1,10 @@
 class User:
     def __init__(self, name, contact_info, address, bank_balance, card_info):
-        self.__name = name
-        self.__contact_info = contact_info
-        self.__address = address
-        self.__bank_balance = bank_balance
-        self.__card_info = card_info
+        self.__name: str = name
+        self.__contact_info: dict = contact_info
+        self.__address: str = address
+        self.__bank_balance: float = bank_balance
+        self.__card_info: str = card_info
 
     def get_name(self):
         return self.__name
@@ -27,7 +27,7 @@ class User:
 class Buisness_user(User):
     def __init__(self, cvr, name, contact_info, address, bank_balance, card_info):
         super().__init__(name= name, contact_info= contact_info, address= address, bank_balance= bank_balance, card_info= card_info)
-        self.__cvr = cvr
+        self.__cvr: str = cvr
 
     def get_CVR(self):
         return self.__cvr
@@ -35,7 +35,7 @@ class Buisness_user(User):
 class Private_user(User):
     def __init__(self, contacts, name, contact_info, address, bank_balance, card_info):
         super().__init__(name= name, contact_info= contact_info, address= address, bank_balance= bank_balance, card_info= card_info)
-        self.__contacts = contacts
+        self.__contacts: list = contacts
 
     def get_contacts(self):
         return self.__contacts
